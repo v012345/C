89384aa456da55f9377efaf1d6b818bb2f29f437
v012345 <v138468@gmail.com>
2023-09-28 16:28:00 +0800

    c 的函数参数与返回函数

--- main.c
+++ main.c
@@ -5,6 +5,22 @@
 #include <string.h>
 #include <stdlib.h>
 
+int x(int a)
+{
+    return a;
+}
+int y(int b)
+{
+    return b + 1;
+}
+
+int (*factory(int (*f)(int)))(int)
+{
+    if (f == x)
+        return x;
+    return y;
+}
+
 int main()
 {
 #ifdef _WIN32 // _WIN32 在 32 和 64 位上都有定义, _WIN64 只在 64 位上有定义
@@ -18,5 +34,7 @@ int main()
     printf("float 最大值: %E\n", FLT_MAX);
     int i = 1;
     i++;
+    int (*a)(int) = factory(y);
+    printf("%d", a(2));
     return 0;
 }
